009610e3cfb824a82a9409dc2763524e33bf6d18
Nalee Riddell <nriddell@tentcraft.com>
2020-06-22 12:00:05 -0400

    barebones with defaults

--- potusScraper.js
+++ potusScraper.js
@@ -1,21 +1,50 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
-const options = {
-	uri: `https://codeburst.io/an-introduction-to-web-scraping-with-node-js-1045b55c63f7`,
-	transform: function (body) {
-	  return cheerio.load(body);
-	}
-  };
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-rp(options)
-  .then(($) => {
-	// console.log($('li').text());
-	$('h1').each((i, el) => {
-		const item = $(el).text();
-		console.log(item);
-	})
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+rl.question("url (empty for default):", function(url) {
+    rl.question("query (empty for default):", function(query) {
+
+		let newUrl = url ? url : "https://naleeriddell.com";
+		const options = {
+			uri: newUrl,
+			transform: function (body) {
+			  return cheerio.load(body);
+			}
+		  };
+		
+		rp(options)
+		  .then(($) => {
+			console.log('\n============\n');
+			// $('li').each((i, el) => {
+			// 	const item = $(el).text();
+			// 	console.log(item);
+			// })
+			// console.log($('li').text());
+			if (query) {
+			eval(query);
+			} else {
+				console.log($('li').text());
+
+			}
+
+			rl.close();
+		  })
+		  .catch((err) => {
+			console.log(err);
+			rl.close();
+		  });
+        // console.log(`${name}, is a citizen of ${country}`);
+    });
+});
+
+rl.on("close", function() {
+	console.log('\n ============');
+    console.log("Thanks for using Web-Scrapey-X");
+    process.exit(0);
+});
